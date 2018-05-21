0e0ff3b82d2ef2bb157e8b59d79ad1d707f9fe9e
Edra Hjaldo <alexandre.jouniot@gmail.com>
2018-05-21 17:06:07 +0200

    Ajout Level1
    J'ai tout écrasé mais c'est good

---
+++ Assets/Scripts/ControllerCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCollision : MonoBehaviour {
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.CompareTag("endTrigger"))
+        {
+            Logic.Instance.GoToNextLevel();
+         
+        }
+    }
+}
