84fbc837d3fbfd9dbdb196ae9e7e34ef832a6b02
Edra Hjaldo <alexandre.jouniot@gmail.com>
2018-06-10 00:51:19 +0200

    Ajout Level2

---
+++ Assets/NextShader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextShader : MonoBehaviour {
+
+    [SerializeField]
+    Material nextMaterialToApply;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
