9b95795862b712a2944a2be9dd931db5e0e8cd6a
Nicsi <nicolas.sirri@edu.esiee.fr>
2018-06-10 14:06:21 +0200

    dalles separables

---
+++ Assets/Scripts/Utils/OnTriggerLoose.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerLoose : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            GameManager.Instance.CallGameLoose();
+    }
+}
