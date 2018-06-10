c8adcc5552b10458a10704b4951ca9dac4cb9c8b
Nicsi <nicolas.sirri@edu.esiee.fr>
2018-06-10 18:27:13 +0200

    Tp Trigger

---
+++ Assets/Scripts/Utils/OnTriggerTeleport.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class OnTriggerTeleport : MonoBehaviour {
+
+    [SerializeField]
+    GameObject m_TeleportPosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if(PlayerManager.isCarryingObject)
+            {
+                GameManager.Instance.CallDropInteractableObject();
+            }
+            PlayerManager.SetPlayerTransform(m_TeleportPosition.transform);
+
+        }
+    }
+
+
+}
