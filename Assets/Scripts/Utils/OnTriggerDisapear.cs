02004a8b020ee0d964d49b25143b150bbbba1ac9
Nicsi <nicolas.sirri@edu.esiee.fr>
2018-06-10 13:18:24 +0200

    OnTriggerDisapear

---
+++ Assets/Scripts/Utils/OnTriggerDisapear.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerDisapear : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("LanternEffect"))
+            gameObject.SetActive(false);
+    }
+}
