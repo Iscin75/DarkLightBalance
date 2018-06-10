43838394fc86b0971a2853ca41f005f50c501cb8
Edra Hjaldo <alexandre.jouniot@gmail.com>
2018-06-10 17:12:15 +0200

    modifs level2

---
+++ Assets/Scripts/Utils/OnTriggerChangeLanternEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class OnTriggerChangeLanternEffect : MonoBehaviour {
+
+    [SerializeField]
+    GameObject AreaOfEffect;
+    [SerializeField]
+    Material toApply;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            AreaOfEffect.GetComponent<Renderer>().material = toApply;
+    }
+}
