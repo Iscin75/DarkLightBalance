3a11ce5ef9795c0e78236642e42b6ce8a57fb54e
Nicsi <nicolas.sirri@edu.esiee.fr>
2018-06-10 11:27:06 +0200

    maj animator

---
+++ Assets/Scripts/Utils/OnTriggerAnimation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class OnTriggerAnimation : MonoBehaviour {
+
+    [SerializeField]
+    GameObject affectedObject;
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = affectedObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collide");
+            anim.SetBool("isOpen",true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collide");
+            anim.SetBool("isOpen", false);
+        }
+    }
+}
